9bbdaf8591bdd40b200b276f1fea86aacb6b60e0
Giorgian Borca-Tasciuc <giorgianb@openmailbox.org>
2018-02-03 13:48:37 -0500

    added appropiate unique columns

--- setup_db.sh
+++ setup_db.sh
@@ -10,28 +10,29 @@ DATABASE_NAME="pickup_game"
 USERS_TABLE_NAME="users"
 GAMES_TABLE_NAME="games"
 
-$MYSQL_COMMAND -h "$MYSQL_HOST" -u "$MYSQL_USER_NAME" --password="$MYSQL_PASSWORD" << END_OF_PICKUP_DB_SCRIPT
+$MYSQL_COMMAND -h "$MYSQL_HOST" -u "$MYSQL_USER_NAME" --password="$MYSQL_PASSWORD"  << END_OF_PICKUP_DB_SCRIPT
   CREATE DATABASE $DATABASE_NAME;
   USE $DATABASE_NAME;
-   CREATE TABLE $USERS_TABLE_NAME (
+  CREATE TABLE $USERS_TABLE_NAME (
   id int(11) NOT NULL AUTO_INCREMENT,
-  email varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
-  user_name varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  email varchar(127) COLLATE utf8mb4_unicode_ci NOT NULL,
+  user_name varchar(127) COLLATE utf8mb4_unicode_ci NOT NULL,
   password_hash varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
   password_salt char(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  UNIQUE (user_name),
   PRIMARY KEY (id)
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-   CREATE TABLE $GAMES_TABLE_NAME (
+  CREATE TABLE $GAMES_TABLE_NAME (
   id int(11) NOT NULL AUTO_INCREMENT,
-  name varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  name varchar(127) COLLATE utf8mb4_unicode_ci NOT NULL,
   host_id int(11) NOT NULL,
   type enum('basketball','baseball','soccer') COLLATE utf8mb4_unicode_ci NOT NULL,
   visibility enum('public','private') COLLATE utf8mb4_unicode_ci NOT NULL,
   latitude double NOT NULL,
   longitude double NOT NULL,
   until date NOT NULL,
+  UNIQUE (host_id),
   PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 END_OF_PICKUP_DB_SCRIPT
